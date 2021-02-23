1d0de536fb20bd1b8e12c5d269f3a43abe123bdc
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2021-02-22 18:13:09 -0600

    added components and finished homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AccountComponent } from './account/account.component';
+import { DetailsComponent } from './details/details.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    ReleasesComponent,
+    CartComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountComponent,
+    DetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
