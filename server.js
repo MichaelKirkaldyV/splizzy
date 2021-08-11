8ba851da63ee383516ba45b4fb65d38e7feb0306
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2021-08-11 14:12:39 -0500

    added dist folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var path = require("path");
 var bodyParser = require('body-parser');
 
-app.use(express.static( __dirname + 'public' ));
+app.use(express.static( __dirname + "/dist" ));
 
 app.use(bodyParser.json());
 
